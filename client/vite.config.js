50e5d66cf7b5740e2485fb7de47339a892a7182b
Chandra0804 <chandrasaiteja0804@gmail.com>
2024-08-12 23:57:17 +0530

    deploy5

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/IOTWorkshop/', // Set this to your repo name
+  base: '/IotWorkshop/', // Set this to your repo name
   build: {
     outDir: 'build',
   },
