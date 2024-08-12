c1f5d08416424e5897190d519d3d64fd6c7b8902
Chandra0804 <chandrasaiteja0804@gmail.com>
2024-08-12 23:51:59 +0530

    deploy3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import LandingPage from './pages/LandingPage'
-import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <>
+    <div className='App'>
     <LandingPage/>
-    </>
+    </div>
   )
 }
 
